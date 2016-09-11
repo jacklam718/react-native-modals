0bd0c181706524c191f525855f91d258bd3068ed
Jack Lam <jacklam718@gmail.com>
2016-09-11 23:25:29 +0800

    create gh-pages

--- index.js
+++
@@ -1,8 +0,0 @@
-import PopupDialog from './src/PopupDialog';
-import Overlay from './src/components/Overlay';
-import Dialog from './src/components/Dialog';
-import Animation from './src/animations/Animation';
-import ScaleAnimation from './src/animations/ScaleAnimation';
-
-export { Overlay, Dialog, Animation, ScaleAnimation };
-export default PopupDialog;
