c7b59c8aa121ce2c52d3e4c64e0556cb91c5d8d3
Jack Lam <jacklam718@gmail.com>
2016-09-11 11:43:16 +0800

    add dialog title

--- src/PopupDialog.js
+++ src/PopupDialog.js
@@ -1,138 +1,60 @@
 // flow
 
-import React, { PropTypes, Component, Element } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import React, { PropTypes, Component } from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
 import Dialog from './components/Dialog';
-import Overlay from './components/Overlay';
+import DialogTitle from './components/DialogTitle';
 import ScaleAnimation from './animations/ScaleAnimation';
 
 const SCRREN_WIDTH = Dimensions.get('window').width;
 const SCRREN_HEIGHT = Dimensions.get('window').height;
 
 const propTypes = {
-  animation: PropTypes.string,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  overlayBackgroundColor: PropTypes.string,
-  overlayOpacity: PropTypes.number,
-  dialogAnimation: PropTypes.object,
-  animationDuration: PropTypes.number,
-  closeOnTouchOutside: PropTypes.bool,
-  open: PropTypes.bool,
-  onOpened: PropTypes.func,
-  onClosed: PropTypes.func,
-  children: PropTypes.any,
+  ...Dialog.propTypes,
+  ...DialogTitle.propTypes,
 };
 
 const defaultProps = {
   animation: 'scale',
   animationDuration: 200,
   closeOnTouchOutside: true,
+  dialogAnimation: new ScaleAnimation(),
 };
 
 class PopupDialog extends Component {
   static propTypes = propTypes
   static defaultProps = defaultProps
 
-  constructor(props) {
-    super(props);
-
-    // get animation from props or create animation from Animtion modules
-    if (props.dialogAnimation) {
-      this.dialogAnimation = props.dialogAnimation;
-    } else if (props.animation === 'scale') {
-      this.dialogAnimation = new ScaleAnimation();
-    }
-
-    // opened, opening, closed, closing,
-    this.state = {
-      dialogState: 'closed',
-    };
-
-    this.onOverlayPress = this.onOverlayPress.bind(this);
+  openDialog(onOpened) {
+    this.dialog.open(onOpened);
   }
 
-  componentDidMount() {
-    if (this.props.open) {
-      this.openDialog(this.props.onOpened);
-    } else {
-      this.closeDialog(this.props.onClosed);
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.open !== nextProps.open) {
-      if (nextProps.open) {
-        return this.openDialog(this.nextProps.onOpened);
-      }
-      return this.closeDialog(this.nextProps.onClosed);
-    }
-    return nextProps;
-  }
-
-  onOverlayPress() {
-    this.closeDialog();
-  }
-
-  setDialogState(toValue, callback) {
-    this.dialogAnimation.toValue(toValue);
-    let dialogState = toValue ? 'opening' : 'closing';
-
-    this.setState({ dialogState });
-
-    setTimeout(() => {
-      dialogState = dialogState === 'closing' ? 'closed' : 'opened';
-      this.setState({ dialogState });
-      if (callback) callback();
-    }, this.props.animationDuration);
-  }
-
-  openDialog(onOpened = this.props.onOpened) {
-    this.setDialogState(1, onOpened);
-  }
-
-  closeDialog(onClosed = this.props.onClosed) {
-    this.setDialogState(0, onClosed);
+  closeDialog(onClosed) {
+    this.dialog.closed(onClosed);
   }
 
   render() {
-    let hidden;
-    let dialog;
+    let title;
 
-    const dialogShow = ['opened', 'opening'].includes(this.state.dialogState);
-
-    if (dialogShow || this.state.dialogState !== 'closed') {
-      dialog = (
-        <Dialog
-          width={this.props.width}
-          height={this.props.height}
-          animations={this.dialogAnimation.animations}
-        >
-          {this.props.children}
-        </Dialog>
-      );
-    } else if (this.state.dialogState === 'closed') {
-      hidden = styles.hidden;
+    if (this.props.title) {
+      title = <DialogTitle {...this.props} />;
     }
 
+
     return (
-      <View style={[styles.popupContainer, hidden]}>
-        <Overlay
-          showOverlay={dialogShow}
-          onPress={this.onOverlayPress}
-          backgroundColor={this.props.overlayBackgroundColor}
-          opacity={this.props.overlayOpacity}
-          animationDuration={this.props.animationDuration}
-        />
-        {dialog}
-      </View>
+      <Dialog
+        ref={(dialog) => { this.dialog = dialog; }}
+        {...this.props}
+      >
+        {title}
+      </Dialog>
     );
   }
 }
 
 const styles = StyleSheet.create({
   popupContainer: {
-    flex: 1,
+    // flex: 1,
     position: 'absolute',
     top: 0,
     left: 0,
--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,63 +1,169 @@
 // flow
 
-import React, { PropTypes, Element, cloneElement, Children } from 'react';
-import { StyleSheet, Animated, Dimensions } from 'react-native';
+import React, { PropTypes, Component } from 'react';
+import { View, StyleSheet, Animated, Dimensions } from 'react-native';
+import Overlay from './Overlay';
 
 const SCRREN_WIDTH = Dimensions.get('window').width;
 const SCRREN_HEIGHT = Dimensions.get('window').height;
 
-function getDialogSize({ width, height }): Object {
-  const size = { width, height };
-  if (width > 0.0 && width < 1.0) {
-    size.width = width * SCRREN_WIDTH;
-  }
-  if (height > 0.0 && height < 1.0) {
-    size.height = height * SCRREN_HEIGHT;
-  }
-  return size;
-}
-
-function Dialog({ animations, width, height, children }): Element {
-  const size = getDialogSize({ width, height });
-  let backgroundColor;
-
-  if (children) {
-    if (children.props.style) {
-      backgroundColor = { backgroundColor: children.props.style.backgroundColor };
-    }
-  }
-
-  return (
-    <Animated.View style={[styles.dialog, size, animations, backgroundColor]} >
-      {children}
-    </Animated.View>
-  );
-}
-
-Dialog.propTypes = {
-  animations: PropTypes.object,
+const propTypes = {
+  animation: PropTypes.string,
   width: PropTypes.number,
   height: PropTypes.number,
+  overlayBackgroundColor: PropTypes.string,
+  overlayOpacity: PropTypes.number,
+  dialogAnimation: PropTypes.object,
+  animationDuration: PropTypes.number,
+  closeOnTouchOutside: PropTypes.bool,
+  open: PropTypes.bool,
+  onOpened: PropTypes.func,
+  onClosed: PropTypes.func,
   children: PropTypes.any,
 };
 
-Dialog.defaultProps = {
+const defaultProps = {
+  animation: 'scale',
+  animationDuration: 200,
   width: SCRREN_WIDTH,
   height: 300,
+  closeOnTouchOutside: true,
 };
 
+class Dialog extends Component {
+  static propTypes = propTypes
+  static defaultProps = defaultProps
+
+  constructor(props) {
+    super(props);
+
+    // get animation from props
+    if (props.dialogAnimation) {
+      this.dialogAnimation = props.dialogAnimation;
+    }
+
+    // opened, opening, closed, closing,
+    this.state = {
+      dialogState: 'closed',
+    };
+
+    this.onOverlayPress = this.onOverlayPress.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.open) {
+      this.open(this.props.onOpened);
+    } else {
+      this.close(this.props.onClosed);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.open !== nextProps.open) {
+      if (nextProps.open) {
+        return this.open(this.nextProps.onOpened);
+      }
+      return this.close(this.nextProps.onClosed);
+    }
+    return nextProps;
+  }
+
+  onOverlayPress() {
+    this.close();
+  }
+
+  setDialogState(toValue, callback) {
+    this.dialogAnimation.toValue(toValue);
+    let dialogState = toValue ? 'opening' : 'closing';
+
+    this.setState({ dialogState });
+
+    setTimeout(() => {
+      dialogState = dialogState === 'closing' ? 'closed' : 'opened';
+      this.setState({ dialogState });
+      if (callback) callback();
+    }, this.props.animationDuration);
+  }
+
+  open(onOpened = this.props.onOpened) {
+    this.setDialogState(1, onOpened);
+  }
+
+  close(onClosed = this.props.onClosed) {
+    this.setDialogState(0, onClosed);
+  }
+
+  getDialogSize({ width, height }): Object {
+    const size = { width, height };
+    if (width > 0.0 && width < 1.0) {
+      size.width = width * SCRREN_WIDTH;
+    }
+    if (height > 0.0 && height < 1.0) {
+      size.height = height * SCRREN_HEIGHT;
+    }
+    return size;
+  }
+
+  render() {
+    let hidden;
+    let dialog;
+
+    const dialogState = this.state.dialogState;
+
+    if (dialogState === 'closed') {
+      hidden = styles.hidden;
+    } else {
+      const size = this.getDialogSize(this.props);
+      let backgroundColor;
+      if (this.props.children) {
+        if (this.props.children.props.style) {
+          backgroundColor = { backgroundColor: this.props.children.props.style.backgroundColor };
+        }
+      }
+
+      dialog = (
+        <Animated.View style={[styles.dialog, size, this.dialogAnimation.animations]} >
+          {this.props.children}
+        </Animated.View>
+      );
+    }
+
+    return (
+      <View style={[styles.popupContainer, hidden]}>
+        <Overlay
+          showOverlay={['opened', 'opening'].includes(dialogState)}
+          onPress={this.onOverlayPress}
+          backgroundColor={this.props.overlayBackgroundColor}
+          opacity={this.props.overlayOpacity}
+          animationDuration={this.props.animationDuration}
+        />
+        {dialog}
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
+  popupContainer: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: SCRREN_WIDTH,
+    height: SCRREN_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   dialog: {
-    width: 300,
-    height: 300,
     borderRadius: 8,
-    padding: 5,
-    // position: 'absolute',
-    // top: 0,
-    // bottom: 0,
-    // justifyContent: 'center',
-    // alignItems: 'center',
+    // padding: 5,
     backgroundColor: '#ffffff',
+  },
+  hidden: {
+    top: -10000,
+    left: 0,
+    height: 0,
+    width: 0,
   },
 });
 
