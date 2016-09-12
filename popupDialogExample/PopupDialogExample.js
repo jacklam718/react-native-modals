6cf4d18cd532d2b0be8c3962c022795b57b4e0c3
Aviad Hahami <aviadhahami@gmail.com>
2016-09-12 13:57:33 +0300

    :bug: Removed the fancy button + it's deps

--- popupDialogExample/PopupDialogExample.js
+++ popupDialogExample/PopupDialogExample.js
@@ -1,47 +1,48 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { RoundButton } from 'react-native-button-component';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
 import PopupDialog from 'react-native-popup-dialog';
 
 export default class PopupDialogExample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dialogOpen: false,
-    };
-
-    this.openDialog = this.openDialog.bind(this);
-  }
-
-  openDialog() {
-    this.popupDialog.openDialog();
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <RoundButton
-          text="Open Dialog"
-          onPress={this.openDialog}
-        />
-        <PopupDialog
-          ref={(popupDialog => { this.popupDialog = popupDialog; })}
-          title="Popup Dialog"
-        >
-          <View>
-            <Text>Hello</Text>
-          </View>
-        </PopupDialog>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			dialogOpen: false,
+		};
+		
+		this.openDialog = this.openDialog.bind(this);
+	}
+	
+	openDialog() {
+		this.popupDialog.openDialog();
+	}
+	
+	render() {
+		return (
+			<View style={styles.container}>
+				<TouchableHighlight
+					text="Open Dialog"
+					onPress={this.openDialog}
+				/>
+				<PopupDialog
+					ref={(popupDialog => {
+						this.popupDialog = popupDialog;
+					})}
+					title="Popup Dialog"
+				>
+					<View>
+						<Text>Hello</Text>
+					</View>
+				</PopupDialog>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: '#000',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		// backgroundColor: '#000',
+	},
 });
