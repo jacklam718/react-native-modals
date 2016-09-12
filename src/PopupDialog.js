4f12a7267695a3e20bbae1847178fc9e8a8c5026
Aviad Hahami <aviadhahami@gmail.com>
2016-09-12 14:27:33 +0300

    :rocket: Indentation + removed unused variables

--- src/PopupDialog.js
+++ src/PopupDialog.js
@@ -1,54 +1,53 @@
 // flow
 
-import React, { PropTypes, Component } from 'react';
-import { Dimensions } from 'react-native';
+import React, {PropTypes, Component} from 'react';
+import {Dimensions} from 'react-native';
 import Dialog from './components/Dialog';
 import DialogTitle from './components/DialogTitle';
 import ScaleAnimation from './animations/ScaleAnimation';
 
-const SCRREN_WIDTH = Dimensions.get('window').width;
-const SCRREN_HEIGHT = Dimensions.get('window').height;
-
 const propTypes = {
-  ...Dialog.propTypes,
-  ...DialogTitle.propTypes,
+	...Dialog.propTypes,
+	...DialogTitle.propTypes,
 };
 
 const defaultProps = {
-  animation: 'scale',
-  animationDuration: 200,
-  closeOnTouchOutside: true,
-  dialogAnimation: new ScaleAnimation(),
+	animation: 'scale',
+	animationDuration: 200,
+	closeOnTouchOutside: true,
+	dialogAnimation: new ScaleAnimation(),
 };
 
 class PopupDialog extends Component {
-  static propTypes = propTypes
-  static defaultProps = defaultProps
-
-  openDialog(onOpened) {
-    this.dialog.open(onOpened);
-  }
-
-  closeDialog(onClosed) {
-    this.dialog.closed(onClosed);
-  }
-
-  render() {
-    let title;
-
-    if (this.props.title) {
-      title = <DialogTitle {...this.props} />;
-    }
-
-    return (
-      <Dialog
-        ref={(dialog) => { this.dialog = dialog; }}
-        {...this.props}
-      >
-        {title}
-      </Dialog>
-    );
-  }
+	static propTypes = propTypes
+	static defaultProps = defaultProps
+	
+	openDialog(onOpened) {
+		this.dialog.open(onOpened);
+	}
+	
+	closeDialog(onClosed) {
+		this.dialog.closed(onClosed);
+	}
+	
+	render() {
+		let title;
+		
+		if (this.props.title) {
+			title = <DialogTitle {...this.props} />;
+		}
+		
+		return (
+			<Dialog
+				ref={(dialog) => {
+					this.dialog = dialog;
+				}}
+				{...this.props}
+			>
+				{title}
+			</Dialog>
+		);
+	}
 }
 
 export default PopupDialog;
