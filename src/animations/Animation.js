6d078f4535e7cfb176a385170ce76642fd091961
Jack Lam <jacklam718@gmail.com>
2016-09-15 21:20:36 +0800

    fix spaces

--- src/animations/Animation.js
+++ src/animations/Animation.js
@@ -4,19 +4,20 @@ import { Animated } from 'react-native';
 
 // Base Animation class
 export default class Animation {
-	constructor(toValue: ?number = 0) {
-		this.animate = new Animated.Value(toValue);
-		this.animations = this.createAnimations();
-	}
-	
-	toValue(toValue: number) {
-	}
-	
-	createAnimations(): Object {
-		return {};
-	}
-	
-	render() {
-		return null;
-	}
+  constructor(toValue: ?number = 0) {
+    this.animate = new Animated.Value(toValue);
+    this.animations = this.createAnimations();
+  }
+
+  toValue(toValue: number) {
+    return toValue;
+  }
+
+  createAnimations(): Object {
+    return {};
+  }
+
+  render() {
+    return null;
+  }
 }
--- src/animations/ScaleAnimation.js
+++ src/animations/ScaleAnimation.js
@@ -1,52 +1,52 @@
 // flow
 
-import {Animated} from 'react-native';
+import { Animated } from 'react-native';
 import Animation from './Animation';
 
 export default class ScaleAnimation extends Animation {
-	toValue(toValue: number, onFinished) {
-		switch (toValue) {
-			case 0:
-				Animated.spring(this.animate, {
-					toValue,   // Returns to the start
-					velocity: 3,  // Velocity makes it move
-					tension: 250, // Slow
-					friction: 20,  // Oscillate a lot
-				}).start(onFinished);
-				break;
-			case 1:
-				Animated.spring(this.animate, {
-					toValue,   // Returns to the start
-					velocity: 0,  // Velocity makes it move
-					tension: 65, // Slow
-					friction: 7,  // Oscillate a lot
-				}).start(onFinished);
-				break;
-			default:
-				break;
-		}
-	}
-	
-	createAnimations() {
-		const transform = [
-			{
-				scale: this.animate.interpolate({
-					inputRange: [0, 1],
-					outputRange: [0, 1],
-				}),
-			},
-			{
-				scale: this.animate.interpolate({
-					inputRange: [0, 1],
-					outputRange: [0, 1],
-				}),
-			},
-		];
-		
-		const animations = {
-			transform,
-		};
-		
-		return animations;
-	}
+  toValue(toValue: number, onFinished) {
+    switch (toValue) {
+      case 0:
+        Animated.spring(this.animate, {
+          toValue,
+          velocity: 3,
+          tension: 250,
+          friction: 20,
+        }).start(onFinished);
+        break;
+      case 1:
+        Animated.spring(this.animate, {
+          toValue,
+          velocity: 0,
+          tension: 65,
+          friction: 7,
+        }).start(onFinished);
+        break;
+      default:
+        break;
+    }
+  }
+
+  createAnimations() {
+    const transform = [
+      {
+        scale: this.animate.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+        }),
+      },
+      {
+        scale: this.animate.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+        }),
+      },
+    ];
+
+    const animations = {
+      transform,
+    };
+
+    return animations;
+  }
 }
