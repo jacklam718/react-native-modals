83a16c8fd2f2ee04d0e4423c5b5854430feb5a07
Jack Lam <jacklam718@gmail.com>
2016-09-16 13:55:38 +0800

    export SlideAnimation on index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Overlay from './src/components/Overlay';
 import Dialog from './src/components/Dialog';
 import Animation from './src/animations/Animation';
 import ScaleAnimation from './src/animations/ScaleAnimation';
+import SlideAnimation from './src/animations/SlideAnimation';
 
-export { Overlay, Dialog, Animation, ScaleAnimation };
+export { Overlay, Dialog, Animation, ScaleAnimation, SlideAnimation };
 export default PopupDialog;
