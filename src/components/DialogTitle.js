f8f93bae556681c20837f7f4b468c5410d2ad46b
Jack Lam <jacklam718@gmail.com>
2016-10-23 03:14:08 +0800

    make the dialogTitle of the title bar show hide able.

--- src/components/DialogTitle.js
+++ src/components/DialogTitle.js
@@ -1,9 +1,20 @@
+// @flow
+
 import React, { PropTypes } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-function DialogTitle({ title, titleStyle, titleTextStyle }) {
+type Param = {
+  title: string | number,
+  titleStyle: Object | number,
+  titleTextStyle: Object | number,
+  haveTitleBar: Boolean,
+};
+
+function DialogTitle({ title, titleStyle, titleTextStyle, haveTitleBar = true }: Param) {
+  const titleBar = haveTitleBar ? styles.titleBar : null;
+
   return (
-    <View style={[styles.title, titleStyle]}>
+    <View style={[styles.title, titleBar, titleStyle]}>
       <Text style={[styles.titleText, titleTextStyle]}>
         {title}
       </Text>
@@ -11,21 +22,18 @@ function DialogTitle({ title, titleStyle, titleTextStyle }) {
   );
 }
 
-DialogTitle.propTypes = {
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  titleStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-  titleTextStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-};
-
 const styles = StyleSheet.create({
   title: {
-    padding: 14,
-    backgroundColor: '#F9F9FB',
+    padding: 24,
     alignItems: 'center',
-    borderColor: '#DAD9DC',
-    borderBottomWidth: 0.5,
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
+  },
+  titleBar: {
+    padding: 14,
+    borderBottomWidth: 0.5,
+    backgroundColor: '#F9F9FB',
+    borderColor: '#DAD9DC',
   },
   titleText: {
     color: '#7F7D89',
