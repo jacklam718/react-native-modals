f4ae54d87bbd0841f2601ea16c7c74de497044ee
Jack Lam <jacklam718@gmail.com>
2017-04-13 14:41:22 +0800

    move index.js to src

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+Object.defineProperty(exports,"__esModule",{value:true});exports.OverlayType=exports.DialogTitleType=exports.DialogButtonType=exports.DialogType=exports.PopupDialogType=exports.SlideAnimation=exports.ScaleAnimation=exports.DefaultAnimation=exports.Animation=exports.Dialog=exports.DialogTitle=exports.DialogButton=exports.Overlay=undefined;var _Type=require('./Type');
+
+
+
+
+
+
+var _PopupDialog=require('./PopupDialog');var _PopupDialog2=_interopRequireDefault(_PopupDialog);
+var _Overlay=require('./components/Overlay');var _Overlay2=_interopRequireDefault(_Overlay);
+var _DialogTitle=require('./components/DialogTitle');var _DialogTitle2=_interopRequireDefault(_DialogTitle);
+var _DialogButton=require('./components/DialogButton');var _DialogButton2=_interopRequireDefault(_DialogButton);
+var _Dialog=require('./components/Dialog');var _Dialog2=_interopRequireDefault(_Dialog);
+var _Animation=require('./animations/Animation');var _Animation2=_interopRequireDefault(_Animation);
+var _DefaultAnimation=require('./animations/DefaultAnimation');var _DefaultAnimation2=_interopRequireDefault(_DefaultAnimation);
+var _ScaleAnimation=require('./animations/ScaleAnimation');var _ScaleAnimation2=_interopRequireDefault(_ScaleAnimation);
+var _SlideAnimation=require('./animations/SlideAnimation');var _SlideAnimation2=_interopRequireDefault(_SlideAnimation);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.
+
+
+Overlay=_Overlay2.default;exports.
+DialogButton=_DialogButton2.default;exports.
+DialogTitle=_DialogTitle2.default;exports.
+Dialog=_Dialog2.default;exports.
+Animation=_Animation2.default;exports.
+DefaultAnimation=_DefaultAnimation2.default;exports.
+ScaleAnimation=_ScaleAnimation2.default;exports.
+SlideAnimation=_SlideAnimation2.default;exports.
+PopupDialogType=_Type.PopupDialogType;exports.
+DialogType=_Type.DialogType;exports.
+DialogButtonType=_Type.DialogButtonType;exports.
+DialogTitleType=_Type.DialogTitleType;exports.
+OverlayType=_Type.OverlayType;exports.default=_PopupDialog2.default;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import {
+  PopupDialogType,
+  DialogType,
+  DialogButtonType,
+  DialogTitleType,
+  OverlayType,
+} from './Type';
+import PopupDialog from './PopupDialog';
+import Overlay from './components/Overlay';
+import DialogTitle from './components/DialogTitle';
+import DialogButton from './components/DialogButton';
+import Dialog from './components/Dialog';
+import Animation from './animations/Animation';
+import DefaultAnimation from './animations/DefaultAnimation';
+import ScaleAnimation from './animations/ScaleAnimation';
+import SlideAnimation from './animations/SlideAnimation';
+
+export {
+  Overlay,
+  DialogButton,
+  DialogTitle,
+  Dialog,
+  Animation,
+  DefaultAnimation,
+  ScaleAnimation,
+  SlideAnimation,
+  PopupDialogType,
+  DialogType,
+  DialogButtonType,
+  DialogTitleType,
+  OverlayType,
+};
+
+export default PopupDialog;
