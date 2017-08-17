f9088ea9c35afd744fb6a092bc5c2e12412dd57f
Jack Lam <jacklam718@gmail.com>
2017-08-17 14:49:52 +0800

    new build and move babel-plugin-flow-react-proptypes to dependencies

--- dist/constants/Constants.js
+++ dist/constants/Constants.js
@@ -1,4 +1,1 @@
-Object.defineProperty(exports,"__esModule",{value:true});var Positions=exports.Positions={
-left:'flex-start',
-right:'flex-end',
-center:'center'};
+Object.defineProperty(exports,"__esModule",{value:true});var Positions=exports.Positions={left:'flex-start',right:'flex-end',center:'center'};
