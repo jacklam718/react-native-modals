64b4340bdd5ba4df97743ae7c0c2e61f91e8e244
jacklam718 <jacklam718@gmail.com>
2018-06-16 19:23:33 +0800

    Cleanup - remove unused code in setup-tests.js and remove unused packages

--- __tests__/setup-tests.js
+++ __tests__/setup-tests.js
@@ -1,30 +1,7 @@
-const { JSDOM } = require('jsdom');
-
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = jsdom;
-
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
-  Object.defineProperties(target, props);
-}
-
-global.window = window;
-global.document = window.document;
-global.navigator = {
-  userAgent: 'node.js',
-};
-copyProps(window, global);
-
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
-
-// Ignore React Web errors when using React Native
-// eslint-disable-next-line
-console.error = message => message;
 
 // Jest doesn't support require.cache, so the correct way to do the mocking is using
 // eslint-disable-next-line
